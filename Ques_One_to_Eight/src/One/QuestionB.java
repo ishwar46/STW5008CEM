58b51f7ad2ada54b25ce679555f498bdd0289565
ishwar46 <ishwarura@gmail.com>
2023-02-26 14:19:54 +0545

    Two and One Update

--- Ques_One_to_Eight/src/One/QuestionB.java
+++ Ques_One_to_Eight/src/One/QuestionB.java
@@ -1,45 +1,109 @@
 package One;
 import java.util.*;
 
-    public class QuestionB {
+import java.util.*;
 
-        public static void main(String[] args) {
-            int[][] edges = {{0,1}, {0,2}, {1,3}, {1,6}, {2,4}, {4,6}, {4,5}, {5,7}};
-            int target = 4;
-            List<Integer> impactedDevices = getImpactedDevices(edges, target);
-            System.out.println("Impacted Device List: " + impactedDevices);
+public class QuestionB {
+
+    // A class to represent a graph.
+    // Size of Matrix will be V (number of vertices in graph) * V
+    private static final int SOURCE_DEVICE = 0;
+    private static final int NETWORK_CONNECTION = 1;
+    private final int V;
+    private final int[][] adjMatrix;
+    private final int faultyDevice;
+
+    // constructor
+    public QuestionB(int V, int[][] networkConnections, int faultyDevice) {
+        //defines the vertices
+        this.V = V;
+        // define the size of array as the number of vertices
+        this.adjMatrix = new int[V][V];
+        //looped over the elements of networkConnections and populate it to Graph
+        for (int[] connection : networkConnections) {
+            // Call AddConnection Function to Populate
+            addConnection(connection[0], connection[1]);
         }
+        //defines the faultyDevice
+        this.faultyDevice = faultyDevice;
+    }
 
-        public static List<Integer> getImpactedDevices(int[][] edges, int target) {
-            Map<Integer, Set<Integer>> adjList = new HashMap<>();
-            for (int[] edge : edges) {
-                int u = edge[0];
-                int v = edge[1];
-                adjList.computeIfAbsent(u, k -> new HashSet<>()).add(v);
-                adjList.computeIfAbsent(v, k -> new HashSet<>()).add(u);
-            }
+    // Adds a connection to an undirected graph
+    public void addConnection(int src, int dest) {
+        // Add a connection from src to dest.
+        adjMatrix[src][dest] = NETWORK_CONNECTION;
+        // Since Network graph is undirected, add a connection from dest to src also
+        adjMatrix[dest][src] = NETWORK_CONNECTION;
+    }
 
-            List<Integer> impactedDevices = new ArrayList<>();
-            boolean[] visited = new boolean[edges.length + 1];
-            dfs(target, visited, adjList);
-            for (int i = 0; i < visited.length; i++) {
-                if (!visited[i]) {
-                    impactedDevices.add(i);
+    // Removes a vertex and all connections connected to it
+    public void removeDevice() {
+        // Remove the device from the matrix
+        for (int i = 0; i < V; i++) {
+            adjMatrix[i][faultyDevice] = 0;
+            adjMatrix[faultyDevice][i] = 0;
+        }
+    }
+
+    // Returns a list of all disconnected devices
+    public List<Integer> getDisconnectedDevices() {
+        //Call removeDevice function to remove the faulty device from Graph
+        removeDevice();
+        // Mark all the vertices as not visited
+        boolean[] visited = new boolean[V];
+        //Defined a List of Integer as subgraphs that  returns disconnected devices
+        List<Integer> disconnectedDevices = new ArrayList<>();
+
+        // Find all disconnected devices
+        for (int v = 0; v < V; ++v) {
+            //Checks if Vertex is Visited or is faulty or not
+            //If Visited, it is not necessary to visit again and also if it is faulty then it automatically separates graphs
+            if (!visited[v] && v != faultyDevice) {
+                // Defined a subgraph for each as there might be multiple small disconnected due to one fault point
+                List<Integer> subgraph = new ArrayList<>();
+                // Print all reachable vertices from v
+                DFSUtil(v, visited, subgraph);
+                // if the Subgraph is the fault point or if the sub
+                //graph has only one vertex, then it is not disconnected
+                if (subgraph.size() > 1 || subgraph.contains(faultyDevice)) {
+                    disconnectedDevices.addAll(subgraph);
                 }
             }
-            return impactedDevices;
         }
+// Add the faulty device back to the graph after finding all disconnected devices
+        addFaultyDeviceBack();
+// Sort the list of disconnected devices
+        Collections.sort(disconnectedDevices);
+        return disconnectedDevices;
+    }
 
-        public static void dfs(int curr, boolean[] visited, Map<Integer, Set<Integer>> adjList) {
-            visited[curr] = true;
-            if (adjList.containsKey(curr)) {
-                for (int neighbor : adjList.get(curr)) {
-                    if (!visited[neighbor]) {
-                        dfs(neighbor, visited, adjList);
-                    }
-                }
+    // A recursive function to print DFS starting from v
+    private void DFSUtil(int v, boolean[] visited, List<Integer> subgraph) {
+        // Mark the current node as visited and add it to the subgraph
+        visited[v] = true;
+        subgraph.add(v);
+        // Recur for all the vertices adjacent to this vertex
+        for (int i = 0; i < V; ++i) {
+            if (adjMatrix[v][i] == NETWORK_CONNECTION && !visited[i]) {
+                DFSUtil(i, visited, subgraph);
             }
         }
     }
+
+    // Adds the faulty device back to the graph
+    private void addFaultyDeviceBack() {
+        for (int i = 0; i < V; i++) {
+            adjMatrix[i][faultyDevice] = NETWORK_CONNECTION;
+            adjMatrix[faultyDevice][i] = NETWORK_CONNECTION;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] networkConnections = {{0, 1}, {0, 2}, {1, 3}, {1, 6}, {2, 4}, {4, 6}, {4, 5}, {5, 7}};
+        int faultyDevice = 4;
+        QuestionB graph = new QuestionB(8, networkConnections, faultyDevice);
+        System.out.println("Disconnected Devices: " + graph.getDisconnectedDevices());
+    }
+}
 
 
---
+++ Ques_One_to_Eight/src/Two/QuestionTwoA.java
@@ -0,0 +1,99 @@
+package Two;
+
+import One.QuestionB;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+//You are given a 2D array containing hierarchical information about certain species,
+//        with edge[i]=[xi,yi], where node xi is connected to xj. You are also provide
+//        d an array of values associated with each species, such that value[i] reflec
+//        ts the ith nodes value. If the greatest common divisor of two values is 1,
+//        they are "relatively prime." Any other node on the shortest path from that
+//        node to the absolute parent node is an ancestor of certain species i. Return
+//        a list of nearest ancestors, where result[i] is the node i's nearest ances
+//        tor such that values[i] and value[result[i]] are both relative primes otherw
+//        ise -1.
+public class QuestionTwoA {
+
+    class Solution {
+        public int gcd(int a, int b) {
+            if (b == 0) {
+                return a;
+            }
+            return gcd(b, a % b);
+        }
+
+        public void dfs(int[] nums, LinkedList<Integer>[] graph, int depth, int node, boolean[] visited, int[] ans, Map<Integer, int[]> ancestorMap, boolean[][] isCoprime) {
+            if (visited[node]) {
+                return;
+            }
+            visited[node] = true;
+            int ancestor = -1;
+            int d = Integer.MAX_VALUE;
+            for (int i = 1; i < 51; i++) {
+                if (isCoprime[nums[node]][i] && ancestorMap.containsKey(i)) {
+                    if (depth - ancestorMap.get(i)[0] <= d) {
+                        d = depth - ancestorMap.get(i)[0];
+                        ancestor = ancestorMap.get(i)[1];
+                    }
+                }
+            }
+            ans[node] = ancestor;
+            int[] exist = (ancestorMap.containsKey(nums[node])) ? ancestorMap.get(nums[node]) : new int[]{-1, -1};
+            ancestorMap.put(nums[node], new int[]{depth, node});
+            for (int child : graph[node]) {
+                if (visited[child]) {
+                    continue;
+                }
+                dfs(nums, graph, depth + 1, child, visited, ans, ancestorMap, isCoprime);
+            }
+            if (exist[0] != -1) {
+                ancestorMap.put(nums[node], exist);
+            } else {
+                ancestorMap.remove(nums[node]);
+            }
+            return;
+        }
+
+        public int[] getCoprimes(int[] nums, int[][] edges) {
+            boolean[][] isCoprime = new boolean[51][51];
+            for (int i = 1; i < 51; i++) {
+                for (int j = 1; j < 51; j++) {
+                    if (gcd(i, j) == 1) {
+                        isCoprime[i][j] = true;
+                        isCoprime[j][i] = true;
+                    }
+                }
+            }
+            int n = nums.length;
+            LinkedList<Integer>[] graph = new LinkedList[n];
+            for (int i = 0; i < graph.length; i++) {
+                graph[i] = new LinkedList<>();
+            }
+            for (int[] edge : edges) {
+                graph[edge[0]].add(edge[1]);
+                graph[edge[1]].add(edge[0]);
+            }
+            int[] ans = new int[n];
+            Arrays.fill(ans, -1);
+            ans[0] = -1;
+            Map<Integer, int[]> ancestorMap = new HashMap<>();
+            boolean[] visited = new boolean[n];
+            dfs(nums, graph, 0, 0, visited, ans, ancestorMap, isCoprime);
+            return ans;
+        }
+    }
+
+    public static void main(String[] args) {
+        QuestionTwoA q = new QuestionTwoA();
+        Solution s = q.new Solution();
+        int[] nums = {5, 6, 10, 2, 3, 6, 15};
+        int[][] edges = {{0, 1}, {0, 2}, {1, 3}, {1, 4}, {2, 5}, {2, 6}};
+        int[] ans = s.getCoprimes(nums, edges);
+        System.out.println("Output : " + Arrays.toString(ans));
+    }
+}
+
