d1c6f2886dce27c130a068e7957aee0faaba41f0
ishwar46 <ishwarura@gmail.com>
2023-02-26 07:45:59 +0545

    8A

---
+++ Ques_One_to_Eight/src/Eight/Question8A.java
@@ -0,0 +1,23 @@
+package Eight;
+public class Question8A {
+
+    public static int getMaxSquareArea(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp = new int[m][n];
+        int maxSquareArea = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    dp[i][j] = 1;
+                    if (i > 0 && j > 0) {
+                        dp[i][j] += Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]);
+                    }
+                    maxSquareArea = Math.max(maxSquareArea, dp[i][j]);
+                }
+            }
+        }
+        return maxSquareArea * maxSquareArea;
+    }
+
+}
