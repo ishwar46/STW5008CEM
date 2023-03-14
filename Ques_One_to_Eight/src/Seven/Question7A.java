56346ae0d376cb1eb96147323cde4f7ebba5e1cb
ishwar46 <ishwarura@gmail.com>
2023-03-14 05:55:02 +0545

    Update Question7A.java

--- Ques_One_to_Eight/src/Seven/Question7A.java
+++ Ques_One_to_Eight/src/Seven/Question7A.java
@@ -1,16 +1,24 @@
 package Seven;
 
-
 public class Question7A {
     public static void main(String[] args) {
-        int[][] A = {{1, 2, 3, 4}, {5, 6, 7, 8}};
-        int[][] B = {{9, 10}, {11, 12}, {13, 14}, {15, 16}};
-        int[][] C = new int[2][2];
-        int numThreads = 2;
+        // Define two matrices A and B
+        int[][] A = {{1, 2, 3, 4}, {5, 6, 7, 8}}; // 2 x 4
+        int[][] B = {{9, 10}, {11, 12}, {13, 14}, {15, 16}}; // 4 x 2
 
+        // Initialize a result matrix C
+        int[][] C = new int[2][2]; // 2 x 2
+
+        // Set the number of threads to be used for matrix multiplication
+        int numThreads = 2; // 2 threads
+
+        // Create an instance of the Question7A class with A, B, C, and numThreads as arguments
         Question7A matrixMultiplication = new Question7A(A, B, C, numThreads);
+
+        // Call the multiply() method to perform matrix multiplication
         matrixMultiplication.multiply();
 
+        // Print the resulting matrix C on the console
         for (int i = 0; i < C.length; i++) {
             for (int j = 0; j < C[0].length; j++) {
                 System.out.print(C[i][j] + " ");
@@ -19,11 +27,13 @@ public class Question7A {
         }
     }
 
+    // Instance variables for matrix A, B, C, and the number of threads
     private int[][] A;
     private int[][] B;
     private int[][] C;
     private int numThreads;
 
+    // Constructor that initializes the instance variables
     public Question7A(int[][] A, int[][] B, int[][] C, int numThreads) {
         this.A = A;
         this.B = B;
@@ -31,15 +41,23 @@ public class Question7A {
         this.numThreads = numThreads;
     }
 
+    // Method to perform matrix multiplication using multiple threads
     public void multiply() {
+        // Divide matrix A into equal-sized chunks based on the number of threads specified
         int numRowsPerThread = A.length / numThreads;
+
+        // Create an array of worker threads
         Thread[] threads = new Thread[numThreads];
+
+        // Create a worker thread for each chunk of matrix A and start the threads
         for (int i = 0; i < numThreads; i++) {
             int startRow = i * numRowsPerThread;
             int endRow = (i == numThreads - 1) ? A.length : startRow + numRowsPerThread;
             threads[i] = new Thread(new Worker(startRow, endRow));
             threads[i].start();
         }
+
+        // Wait for all the worker threads to finish
         for (int i = 0; i < numThreads; i++) {
             try {
                 threads[i].join();
@@ -49,20 +67,23 @@ public class Question7A {
         }
     }
 
+    // Inner class that represents a worker thread
     private class Worker implements Runnable {
         private int startRow;
         private int endRow;
 
+        // Constructor that initializes the starting and ending row indices for the chunk of matrix A
         public Worker(int startRow, int endRow) {
             this.startRow = startRow;
             this.endRow = endRow;
         }
 
+        // Method that performs matrix multiplication for the chunk of matrix A assigned to the worker thread
         @Override
         public void run() {
             for (int i = startRow; i < endRow; i++) {
                 for (int j = 0; j < B[0].length; j++) {
-                    for (int k = 0; k < A[0].length; k++) {
+                    for (int k = 0; k < B.length; k++) {
                         C[i][j] += A[i][k] * B[k][j];
                     }
                 }
@@ -70,5 +91,3 @@ public class Question7A {
         }
     }
 }
-
-
